7c4b278c634cdf5b72ecb20325a869bd242c1ded
artlands <lijie0416@gmail.com>
2025-09-09 12:28:58 -0500

    add stable diffusion

---
+++ hello-world/mpi/mpi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=MPI_TEST_JOB
+#SBATCH --partition=zen4
+#SBATCH --ntasks=16
+#SBATCH --time=00:05:00
+#SBATCH --output=out.%j
+#SBATCH --error=err.%j
+
+module load openmpi/5.0.8
+
+srun ./hello-mpi
