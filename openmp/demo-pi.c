931c15153b9ee48db3aa80b51121191252cc72f5
artlands <lijie0416@gmail.com>
2025-10-23 15:35:22 -0500

    update demo PI

--- openmp/demo-pi.c
+++ openmp/demo-pi.c
@@ -17,20 +17,28 @@ int main(int argc, char **argv) {
 
     double step = 1.0 / (double)steps;
     double sum = 0.0;
+    int used_threads = 1;
 
     double start = omp_get_wtime();
 
-    #pragma omp parallel for reduction(+:sum)
-    for (long i = 0; i < steps; ++i) {
-        double x = (i + 0.5) * step;
-        sum += 4.0 / (1.0 + x * x);
+    #pragma omp parallel //if (steps > 1000000) num_threads(8)
+    {
+        int team = omp_get_num_threads();
+        #pragma omp single
+        { used_threads = team; }
+
+        #pragma omp for reduction(+:sum)
+        for (long i = 0; i < steps; ++i) {
+            double x = (i + 0.5) * step;
+            sum += 4.0 / (1.0 + x * x);
+        }
     }
 
     double pi = sum * step;
     double elapsed = omp_get_wtime() - start;
 
     printf("steps = %ld, pi approx %.12f\n", steps, pi);
-    printf("elapsed = %.6f seconds with %d threads\n", elapsed, omp_get_max_threads());
+    printf("elapsed = %.6f seconds with %d threads\n", elapsed, used_threads);
 
     return 0;
 }
